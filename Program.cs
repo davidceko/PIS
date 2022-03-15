a7e38020a77b67de5891103cc5b602c1b1f07902
davidceko <cekodavid@gmail.com>
2022-03-15 16:42:14 +0100

    treci commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Novo");
+            Console.WriteLine("Novo2");
+
+
         }
     }
 }
